2df4513c90670095fc3909b209b5371712081f2a
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-04-28 22:04:49 +0530

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      // Required for expo-router
+      "react-native-reanimated/plugin",
+    ]
+  };
+};
